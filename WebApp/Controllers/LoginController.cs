f8dfa0085692a12f88676c535beaddf5af70ae8d
Juttahir <juttahir@hotmail.com>
2022-09-04 15:13:51 -0300

    Adicionando projeto Web Application

---
+++ WebApp/Controllers/LoginController.cs
@@ -0,0 +1,61 @@
+﻿using Domain.Interfaces.Repositories;
+using Domain.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApp.Controllers
+{
+    public class LoginController : Controller
+    {
+        private readonly IUsuarioRepository _usuarioService;
+
+        public LoginController(IUsuarioRepository usuarioService)
+        {
+            _usuarioService = usuarioService;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Cadastro()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult ObterUsuario(string email, string senha)
+        {
+            try
+            {
+                var user = _usuarioService.ObterUsuario(email, senha);
+                if(user == null)
+                {
+                    ViewBag.ShowError = true;
+                    return View("Index");
+                }
+
+                return RedirectToAction("Index", "Home");
+            }
+            catch(Exception ex)
+            {
+                return View("Index", ex);
+            }
+        }
+
+        [HttpPost]
+        public IActionResult SalvaUsuario(UsuarioModel usuario)
+        {
+            try
+            {
+                _usuarioService.SalvarUsuario(usuario);
+                ViewBag.Sucesso = true;
+                return View("Index");
+            }
+            catch (Exception ex)
+            {
+                return View("Cadastro", ex);
+            }
+        }
+    }
+}
---
+++ WebApp/wwwroot/js/site.js
@@ -0,0 +1,19 @@
+﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your JavaScript code.
+
+var doShowError = @ViewBag.ShowError.ToString().ToLower();
+
+$(document).ready(function () {
+    $('#btnSubmit').click(showError);
+
+    $('#linkClose').click(function () {
+        $('#errorAlert').hide('fade');
+    });
+
+    // show the error
+    if (doShowError) {
+        showError();
+    }
+});
